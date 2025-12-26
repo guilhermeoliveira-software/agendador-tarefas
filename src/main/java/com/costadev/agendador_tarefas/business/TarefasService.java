95703c20d1b6fbcbdd4cadf78c8c0b16106e2e55
COSTA Dev <guilhermeoliveira.tech@outlook.com.br ]>
2025-12-26 20:10:49 -0300

    adicionados os metodos Get por email e por periodo

--- src/main/java/com/costadev/agendador_tarefas/business/TarefasService.java
+++ src/main/java/com/costadev/agendador_tarefas/business/TarefasService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +28,17 @@ public class TarefasService {
 
                 TarefasEntity entity = tarefasConverter.paraTarefasEntity(dto);
                 return tarefasConverter.paraTarefaDTO(tarefasRepository.save(entity));
+        }
+
+        public List<TarefasDTO> buscarTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
+                return tarefasConverter.paraListaTarefasDTO(
+                        tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+        }
+
+        public List<TarefasDTO> buscarTarefasPorEmail(String token ){
+                String email = jwtUtil.extrairEmailToken(token.substring(7));
+                List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+
+                return tarefasConverter.paraListaTarefasDTO(listaTarefas);
         }
 }
--- src/main/java/com/costadev/agendador_tarefas/controller/TarefasController.java
+++ src/main/java/com/costadev/agendador_tarefas/controller/TarefasController.java
@@ -3,9 +3,14 @@ package com.costadev.agendador_tarefas.controller;
 
 import com.costadev.agendador_tarefas.business.TarefasService;
 import com.costadev.agendador_tarefas.business.dto.TarefasDTO;
+import com.costadev.agendador_tarefas.infrasctruture.entity.TarefasEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -18,5 +23,19 @@ public class TarefasController {
     public ResponseEntity<TarefasDTO> gravarTarefas(@RequestBody TarefasDTO dto,
                                                     @RequestHeader ("Authorization")  String token) {
         return ResponseEntity.ok(tarefasService.gravarTarefas(token, dto));
+    }
+
+    @GetMapping("/eventos")
+    public ResponseEntity<List<TarefasDTO>> buscaListaDeTarefasPorPeriodo(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal){
+
+        return ResponseEntity.ok(tarefasService.buscarTarefasAgendadasPorPeriodo(dataInicial, dataFinal));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TarefasDTO>> buscarTarefasPorEmail(
+            @RequestHeader ("Authorization") String token){
+        return ResponseEntity.ok(tarefasService.buscarTarefasPorEmail(token));
     }
 }
